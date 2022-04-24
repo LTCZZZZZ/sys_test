e2d89c33a17378beda0161863ca4ed89444f3c75
LTC <luodeqin@honghe-tech.com>
2022-04-24 17:31:58 +0800

    常规注释及改动

--- test/max_min_test.py
+++ test/max_min_test.py
@@ -1,9 +1,12 @@
+import os, sys
+# print(sys.path)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from tools import Timer
 
 timer = Timer()
 timer.start()
 
-# 2.650378 sec
+# 2.650378 sec，用pypy运行，只需要0.062 sec
 a = range(1000000)
 for k in range(100):
     print(max(a))
@@ -11,6 +14,7 @@ for k in range(100):
 # 5.811684 sec，在Go那边运行，带上生成的代码，居然才要0.190279 sec，
 # 不带生成的代码，只需要0.061451 sec，也就是说快了94倍左右
 # Go数据类型改为int32还能更快，0.057557s
+# 但下面这段用pypy运行，居然需要12s，反而变慢了，不知为何
 # a = range(1000000, 0, -1)
 # for k in range(100):
 #     big = 0
--- tools/common.py
+++ tools/common.py
@@ -1,5 +1,5 @@
 import time
-import numpy as np
+# import numpy as np
 
 
 class Timer:  # @save
@@ -14,7 +14,7 @@ class Timer:  # @save
         self.tik = time.time()
 
     def record(self):
-        """打点记录时间。"""
+        """打点记录时间。不停歇，self.times列表中记录每一段时间的时长。"""
         self.times.append(time.time() - self.tik)
         if len(self.times) == 1:
             return self.times[-1]
@@ -22,18 +22,19 @@ class Timer:  # @save
             return self.times[-1] - self.times[-2]
 
     def stop(self):
-        """停止计时器并将时间记录在列表中。"""
+        """停止计时器并将时间记录在列表中。record记录分段时长，stop记录累计时长。"""
         self.times.append(time.time() - self.tik)
         return self.times[-1]
 
-    def avg(self):
-        """返回平均时间。"""
-        return sum(self.times) / len(self.times)
-
-    def sum(self):
-        """返回时间总和。"""
-        return sum(self.times)
-
-    def cumsum(self):
-        """返回累计时间。"""
-        return np.array(self.times).cumsum().tolist()
+    # 下面这三个函数和record冲突
+    # def avg(self):
+    #     """返回平均时间。"""
+    #     return sum(self.times) / len(self.times)
+    #
+    # def sum(self):
+    #     """返回时间总和。"""
+    #     return sum(self.times)
+    #
+    # def cumsum(self):
+    #     """返回累计时间。"""
+    #     return np.array(self.times).cumsum().tolist()
