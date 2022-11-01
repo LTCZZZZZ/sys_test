320d990225c558ab878bd9659539aa18f69620a2
LTC <>
2022-11-01 15:34:59 +0800

    Copilot简单示例

--- Copilot/date.py
+++ Copilot/date.py
@@ -1,25 +1,22 @@
 import datetime
-import time
+
 
 # 获取两个日期之间的天数
-def get_days(date1, date2):
-    date1 = datetime.datetime.strptime(date1, "%Y-%m-%d")
-    date2 = datetime.datetime.strptime(date2, "%Y-%m-%d")
+def get_days_between(date1, date2):
+    date1 = datetime.datetime.strptime(date1, '%Y-%m-%d')
+    date2 = datetime.datetime.strptime(date2, '%Y-%m-%d')
     return (date2 - date1).days
 
 
-print(get_days('2019-01-01', '2020-01-02'))
+print(get_days_between('2019-01-01', '2019-02-02'))
 
 
-# 获取两个日期之间的所有日期字符串
-def get_days_list(date1, date2):
-    date1 = datetime.datetime.strptime(date1, "%Y-%m-%d")
-    date2 = datetime.datetime.strptime(date2, "%Y-%m-%d")
-    days = []
-    while date1 <= date2:
-        days.append(date1.strftime('%Y-%m-%d'))
-        date1 += datetime.timedelta(days=1)
-    return days
+# 获取两个日期之间的所有日期，返回datetime类型
+def get_days_between_datetime(date1, date2):
+    date1 = datetime.datetime.strptime(date1, '%Y-%m-%d')
+    date2 = datetime.datetime.strptime(date2, '%Y-%m-%d')
+    days = (date2 - date1).days
+    return [date1 + datetime.timedelta(days=i) for i in range(days + 1)]
 
 
-print(get_days_list('2019-01-01', '2020-01-02'))
+print(get_days_between_datetime('2019-01-01', '2019-02-02'))
--- Copilot/parse_expenses.py
+++ Copilot/parse_expenses.py
@@ -4,9 +4,9 @@ import datetime
 
 def parse_expenses(expenses_string):
     """
-    Parse the list of expenses and return the list of triples (date, value, currency).
-    Ignore lines starting with #.
-    Parse the date using datetime.
+    解析费用列表并返回元组 (date, value, currency)的列表
+    忽略以#开头的行
+    将date转为timestamp并取整
     Example expenses_string:
         2016-01-02 -34.01 USD
         2016-01-03 2.59 DKK
@@ -17,5 +17,13 @@ def parse_expenses(expenses_string):
         if line.startswith('#'):
             continue
         date, value, currency = line.split()
-        expenses.append((datetime.datetime.strptime(date, '%Y-%m-%d'), float(value), currency))
+        date = int(datetime.datetime.strptime(date, '%Y-%m-%d').timestamp())
+        expenses.append((date, float(value), currency))
     return expenses
+
+
+if __name__ == '__main__':
+    res = parse_expenses('''2016-01-02 -34.01 USD
+        2016-01-03 2.59 DKK
+        2016-01-03 -2.72 EUR''')
+    print(res)
