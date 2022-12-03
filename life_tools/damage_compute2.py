de2fcdf3837173f50847d946ca82a27dbb64d4b7
LTC <>
2022-12-03 08:02:27 +0800

    90级提升17%

--- life_tools/damage_compute2.py
+++ life_tools/damage_compute2.py
@@ -17,6 +17,8 @@ def total_simple(atk=1754, cri_r=0.8, cri_d=1.6, ele=0.435):
 # total_simple()
 a = total_simple(atk=2319, cri_r=0.814, cri_d=1.744, ele=0.466+0.12+0.3375)
 b = total_simple(atk=1754, cri_r=0.814, cri_d=2.347, ele=0.466+0.48+0.3375)  # 神乐之真意80级
+c = total_simple(atk=1877, cri_r=0.814, cri_d=2.405, ele=0.466+0.48+0.3375)  # 神乐之真意90级
 # total_simple(cri_r=0.05, cri_d=0.5, ele=0)
 
 print(b / a)
+print(c / a)
