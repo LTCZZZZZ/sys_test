8ec8baa6d3db182e3a78d5992602e117547bb0a1
LTC <>
2023-05-25 15:54:26 +0800

    fix: 测试添加中间行

--- test/git_test.py
+++ test/git_test.py
@@ -1,10 +1,15 @@
 # 测试git冲突
 import numpy as np
 
-print('master')
+print('build-01')
 
 
 def func2():
     for i in range(10):
         print(i)
+    print('insert line')
     print('func2')
+
+
+# this is added content
+print('added content')
