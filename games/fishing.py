b9775061cf5a05bb6a195aee0b07851eb7cdb276
LTC <>
2023-08-02 16:46:25 +0800

    fix: 麻将计算，解决bug，并简单优化版

--- games/fishing.py
+++ games/fishing.py
@@ -1,6 +1,8 @@
+# 特别注意：set函数得到的结果是没排序的，在需要的场景下要使用sorted函数进行排序
+
 # 问：麻将只听2478的牌型存在吗？2678呢？
 # 先考虑听2478而不考虑只听
-
+import time
 # 获取一副牌的听牌（如果没听返回None）：
 # 思路1：从现有牌开始组合，看缺的一张
 # 思路2：遍历加入所有单张牌，判断是否胡牌
@@ -72,18 +74,27 @@ def is_complete(cards):
 
     cards = [int(i) for i in cards]
 
+    # 统计递推次数
+    recursion = 0
+
     def no_pair(cards):
         """
         判断cards是否组成了不含对子的一般型，有几种组法
         :param cards:
         :return:
         """
+        nonlocal recursion
+        recursion += 1
+        print(cards)
+
         complete = 0
         if len(cards) == 0:
             return 1  # 处理边界条件，cards为空时，说明前面的已经全部匹配，返回1
         if len(cards) not in no_pair_num:
             return complete
-        for c in cards:
+
+        # 这里使用set(cards)，因为如果整体匹配到了，直接返回成功，而匹配失败的话，也不需要用同样的数字再来一次
+        for c in sorted(set(cards)):
             for c_match in get_match(c):
                 # c_match是一种含c的面子，如[1, 2, 3]
                 # print(cards, c, c_match)
@@ -92,6 +103,11 @@ def is_complete(cards):
                 if signal:
                     complete += no_pair(sub_cards)
                     # print(f'complete: {complete}')
+
+                # 如果要追求速度，加上以下代码，即只要匹配到就返回，不再继续匹配
+                if complete:
+                    return complete  # 此时no_pair返回的complete最多为1，因为只要匹配到就返回了
+
         # print(cards, complete)
         return complete
 
@@ -101,26 +117,37 @@ def is_complete(cards):
         :param cards:
         :return:
         """
-        # 不要修改原来的cards
-        cards = deepcopy(cards)
 
         if len(cards) in pair_num:
             # 使用set去重
-            for c in set(cards):
+            for c in sorted(set(cards)):
                 # print(cards, c)
                 if cards.count(c) >= 2:
-                    cards.remove(c)
-                    cards.remove(c)
-                    yield cards
+
+                    # 不要修改原来的cards
+                    cards_copy = deepcopy(cards)
+
+                    cards_copy.remove(c)
+                    cards_copy.remove(c)
+                    # print(cards_copy)
+                    yield cards_copy
 
     # 找出所有成型的牌，并计数
     complete = 0
     if len(cards) in pair_num:
         for cs in eliminate_pair_generator(cards):
             complete += no_pair(cs)
+
+            # 如果要追求速度，加上以下代码，即只要匹配到就返回，不再继续匹配
+            # 九莲宝灯速度从1.3s左右下降到0.4s左右，但感觉还是不够快，这个算法似乎已经很难优化了
+            # 想要进一步加速，可以考虑更换算法或使用静态语言，之后用Go试试
+            if complete:
+                break
     else:
         complete += no_pair(cards)
 
+    # 打印递归次数
+    print(f'{cards}: {recursion}')
     return complete
 
 
@@ -128,7 +155,7 @@ def fishing(cards):
     """
     只考虑单种数牌、一般型，计算听牌面数，能向上加1张组成一种型即可，称为成型
     默认cards是已排序过的
-    如3-3，34-25，3334-245，3334567-24578
+    如3-3，34-25，3334-245，3334567-24578，3456667-2578
     :param cards:
     :return:
     """
@@ -137,6 +164,7 @@ def fishing(cards):
     # 成型后不含对子时成型前牌的张数：2，2+3，2+3+3，2+3+3+3
     # no_pair_num = [2, 5, 8, 11]
 
+    time0 = time.time()
     cards = [int(i) for i in cards]
 
     listen = []
@@ -145,22 +173,25 @@ def fishing(cards):
         cards_copy.sort()
         if is_complete(cards_copy):
             listen.append(i)
-    return listen
+
+    time_cost = time.time() - time0
+    return listen, f'{time_cost:.3f} s'
 
 
 if __name__ == '__main__':
-    cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     # print(cards_pop(cards, [1, 2, 3]))
     # print(cards)
 
     # print(is_complete([1, 2, 3]))
-    print(is_complete([1, 2, 3, 4, 5, 6, 7, 8, 9]))  # 162
+    # print(is_complete([1, 2, 3, 4, 5, 6, 7, 8, 9]))  # 优化前：162  # 后，递归次数4
 
-    for v in ['33', '234', '33344', '33345', '33345567']:
-        print(v, is_complete(v))
+    print(is_complete('124567'))
+    # print(is_complete('112345678999'))
+    # print(is_complete('11123345678999'))  # 递归次数5
 
-    for v in ['3', '34', '3334', '3334567']:
-        print(v, fishing(v))
+    # for v in ['33', '234', '33344', '33345', '33345567']:
+    #     print(v, is_complete(v))
 
-
-
+    # for v in ['3', '34', '3334', '3334567', '3456667', '1113455678999', '1112345678999']:
+    #     print(v, fishing(v))
