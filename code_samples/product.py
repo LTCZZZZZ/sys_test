f0c0286c4c3fc8299ef521edf70d2caa4b99621a
LTC <>
2023-08-08 10:58:34 +0800

    feat: 手动实现product函数

--- code_samples/product.py
+++ code_samples/product.py
@@ -1,20 +1,35 @@
+# 手动实现product函数
 # 笛卡尔积，密码生成
-import itertools
+from itertools import product
 
 # rowlists = [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
 # for combination in itertools.product(*rowlists):  # 注意这里的传参，是*传进去的
 #     print(combination)
 
-row = [1, 2]
-print(itertools.product(row, repeat=3))
 
-for comb in itertools.product(row, row, row):
-    print(comb)
+def prod(*args, repeat=1):
+    """
+    手动实现product函数
+    :param args: 传入的列表
+    :param repeat: 重复次数
+    :return:
+    """
+    pools = [tuple(pool) for pool in args] * repeat
+    # print(pools)
+    result = [[]]
+    for pool in pools:
+        # 真的太简洁了，就一行代码
+        result = [x + [y] for x in result for y in pool]
+        # print(result)
+    for v in result:
+        yield tuple(v)
 
-print()
 
-for comb in itertools.product(row, repeat=3):
-    print(comb)
-
-# for comb in itertools.product(row):
-#     print(comb)
+if __name__ == '__main__':
+    row = [1, 2]
+    # temp = product(row, row, row)
+    temp = product(row, repeat=3)
+    print(list(temp))
+    temp = prod(row, repeat=3)
+    print(list(temp))
+    assert list(prod(row, repeat=3)) == list(prod(row, row, row)) == list(product(row, repeat=3))
